9569edde8e643a2ad86146c172f084963887c0d2
Furim <furimmm@furimmm.home>
2021-05-05 17:39:43 +0200

    First files for verison alpha 0.1

---
+++ script.sh
@@ -0,0 +1,11 @@
+!#/bin/bash
+
+rm -r iso_root/myos.elf
+make clean
+make
+cp -v yerbaos.elf iso_root/ 
+
+xorriso -as mkisofs -b limine-cd.bin -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e limine-eltorito-efi.bin -no-emul-boot iso_root -o image.iso
+
+
+qemu-system-x86_64 -cdrom image.iso
